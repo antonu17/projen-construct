c813c3542d87e1dd2eed113bcd4de0f908e4fc32
Anton Ustyuzhanin <antonu17@gmail.com>
2025-04-29 20:58:24 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Anton Ustyuzhanin',
+  authorAddress: 'antonu17@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.8.0',
+  name: 'projen-construct',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/antonu17/projen-construct.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
